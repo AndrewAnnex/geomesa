AndrewAnnex/geomesa

commit 37d9e4a25fcc5b4b4e574e11db97d407e71290a3

GEOMESA-462,463 Decoders and Formatters
wrote decoders and formatters for Raster related Bands and Resolution in Scientific Notation
made resolution a lexicoded double using mango

geomesa-core/src/main/scala/org/locationtech/geomesa/core/index/IndexSchema.scala
@@ -131,6 +131,8 @@ object IndexSchema extends RegexParsers with Logging {
   val RANDOM_CODE = "r"
   val SEPARATOR_CODE = "s"
   val ID_CODE = "id"
+  val RESOLUTION_CODE = "ires"
+  val BAND_CODE = "b"
   val PART_DELIMITER = "::"
 
   def pattern[T](p: => Parser[T], code: String): Parser[T] = CODE_START ~> p <~ (CODE_END + code)
@@ -168,9 +170,24 @@ object IndexSchema extends RegexParsers with Logging {
     case str => ConstantTextFormatter(str)
   }
 
+  // An Image Resolution encoder. '1.8e+10#ires' would yield something
+  // this may not be workable
+  def resolutionPattern = pattern("[^%#]+".r, RESOLUTION_CODE)
+  def resolutionEncoder: Parser[ScientificNotationTextFormatter] = resolutionPattern ^^ {
+    case d => ScientificNotationTextFormatter(lexiDecodeStringToDouble(d))
+  }
+
+  // A Band encoder. 'RGB#b' would yield RGB
+  //  We match any string other that does *not* contain % or # since we use those for delimiters
+  def bandPattern = pattern("[^%#]+".r, BAND_CODE)
+  def bandEncoder: Parser[RasterBandTextFormatter] = bandPattern ^^ {
+    case b => RasterBandTextFormatter(b)
+  }
+
   // a key element consists of a separator and any number of random partitions, geohashes, and dates
   def keypart: Parser[CompositeTextFormatter] =
-    (sep ~ rep(randEncoder | geohashEncoder | dateEncoder | constantStringEncoder)) ^^ {
+    (sep ~ rep(randEncoder | geohashEncoder | dateEncoder | constantStringEncoder | resolutionEncoder | bandEncoder)) ^^
+    {
       case sep ~ xs => CompositeTextFormatter(xs, sep)
     }
 
@@ -292,8 +309,16 @@ object IndexSchema extends RegexParsers with Logging {
     case o ~ b => GeoHashKeyPlanner(o, b)
   }
 
+  def resolutionKeyPlanner: Parser[ResolutionPlanner] = resolutionPattern ^^ {
+    case d => ResolutionPlanner(lexiDecodeStringToDouble(d))
+  }
+
+  def bandKeyPlanner: Parser[BandPlanner] = bandPattern ^^ {
+    case b => BandPlanner(b)
+  }
+
   def keyPlanner: Parser[KeyPlanner] =
-    sep ~ rep(constStringPlanner | datePlanner | randPartitionPlanner | geohashKeyPlanner) <~ "::.*".r ^^ {
+    sep ~ rep(constStringPlanner | datePlanner | randPartitionPlanner | geohashKeyPlanner | resolutionKeyPlanner | bandKeyPlanner) <~ "::.*".r ^^ {
       case sep ~ list => CompositePlanner(list, sep)
     }
 
@@ -303,7 +328,7 @@ object IndexSchema extends RegexParsers with Logging {
   }
 
 
-  def geohashColumnFamilyPlanner: Parser[GeoHashColumnFamilyPlanner] = (keypart ~ PART_DELIMITER) ~> (sep ~ rep(randEncoder | geohashEncoder | dateEncoder | constantStringEncoder)) <~ (PART_DELIMITER ~ keypart) ^^ {
+  def geohashColumnFamilyPlanner: Parser[GeoHashColumnFamilyPlanner] = (keypart ~ PART_DELIMITER) ~> (sep ~ rep(randEncoder | geohashEncoder | dateEncoder | constantStringEncoder | bandKeyPlanner)) <~ (PART_DELIMITER ~ keypart) ^^ {
     case sep ~ xs => xs.find(tf => tf match {
       case gh: GeoHashTextFormatter => true
       case _ => false
@@ -414,6 +439,29 @@ class IndexSchemaBuilder(separator: String) {
       append(ID_CODE)
     }
   }
+
+  /**
+   * Add an Raster Resolution Value
+   *
+   * @return the schema builder instance
+   */
+  def resolution(): IndexSchemaBuilder = append(RESOLUTION_CODE, 0.0)
+
+  /**
+   * Adds a resolution value.
+   *
+   * @param res
+   * @return the schema builder instance
+   */
+  def resolution(res: Double): IndexSchemaBuilder = append(RESOLUTION_CODE, lexiEncodeDoubleToString(res))
+
+  /**
+   * Add a Band Value
+   *
+   * @param band
+   * @return the schema builder instance
+   */
+  def band(band: String): IndexSchemaBuilder = append(BAND_CODE, band)
 
   /**
    * End the current part of the schema format. Schemas consist of (in order) key part, column

geomesa-core/src/main/scala/org/locationtech/geomesa/core/index/index.scala
@@ -21,6 +21,7 @@ import java.util.{List => JList}
 import com.typesafe.scalalogging.slf4j.Logging
 import org.apache.accumulo.core.data.{Key, Value, Range => AccRange}
 import org.geotools.data.Query
+import org.calrissian.mango.types.LexiTypeEncoders
 import org.geotools.factory.Hints.{ClassKey, IntegerKey}
 import org.geotools.filter.identity.FeatureIdImpl
 import org.geotools.geometry.jts.ReferencedEnvelope
@@ -130,5 +131,11 @@ package object index {
       logger.trace(s)
     }
   }
+
+  val doubleAlias = "double"//Double.getClass.getSimpleName.toLowerCase(Locale.US)
+
+  def lexiEncodeDoubleToString(number: Double): String = LexiTypeEncoders.LEXI_TYPES.encode(number)
+
+  def lexiDecodeStringToDouble(str: String): Double = LexiTypeEncoders.LEXI_TYPES.decode(doubleAlias, str).asInstanceOf[Double]
 }
 

geomesa-core/src/test/scala/org/locationtech/geomesa/core/index/FormattersTest.scala
@@ -43,4 +43,25 @@ class FormattersTest extends Specification {
       shardNumbers.size must be equalTo 1
     }
   }
+
+  "ScientificNotationTextFormatter" should {
+    val testNumber = 123456789
+    val scientificNumberFormatter = ScientificNotationTextFormatter(testNumber)
+
+    "map a double to a string representation" in {
+      val scientificText = scientificNumberFormatter.formatString(null, null, null)
+      scientificText must be equalTo "1.234568e+08"
+    }
+  }
+
+  "RasterBandTextFormatter" should {
+    val testBand = "RGB"
+    val bandTextFormatter = RasterBandTextFormatter(testBand)
+
+    "map a string band name to the same string representation" in {
+      val bandText = bandTextFormatter.formatString(null, null, null)
+      bandText must be equalTo "RGB"
+    }
+  }
+
 }

geomesa-core/src/main/scala/org/locationtech/geomesa/core/index/Decoders.scala
@@ -20,6 +20,7 @@ import org.apache.accumulo.core.data.Key
 import org.joda.time.{DateTime, DateTimeZone}
 import org.locationtech.geomesa.utils.geohash.GeoHash
 
+
 trait Decoder[T] {
   def decode(key: Key): T
 }
@@ -42,4 +43,12 @@ case class DateDecoder(orderSeq: Seq[TextExtractor], fs: String) extends Extract
 
 case class IdDecoder(orderedSeq: Seq[TextExtractor]) extends ExtractingDecoder[String] {
   def decode(key: Key): String = seqExtract(orderedSeq, key).replaceAll("_+$", "")
+}
+
+case class ScientificNotationDecoder(orderSeq: Seq[TextExtractor]) extends ExtractingDecoder[Double] {
+  def decode(key: Key): Double = lexiDecodeStringToDouble(seqExtract(orderSeq, key))
+}
+
+case class RasterBandDecoder(orderSeq: Seq[TextExtractor]) extends ExtractingDecoder[String] {
+  def decode(key: Key): String = seqExtract(orderSeq, key)
 }

geomesa-core/src/main/scala/org/locationtech/geomesa/core/index/Formatters.scala
@@ -20,7 +20,6 @@ import org.apache.hadoop.io.Text
 import org.joda.time.DateTime
 import org.locationtech.geomesa.utils.geohash.GeoHash
 import org.opengis.feature.simple.SimpleFeature
-
 import scala.util.hashing.MurmurHash3
 
 trait TextFormatter {
@@ -115,3 +114,27 @@ case class CompositeTextFormatter(lf: Seq[TextFormatter], sep: String) extends T
   def formatString(gh: GeoHash, dt: DateTime, sf: SimpleFeature) = lf.map { _.formatString(gh, dt, sf) }.mkString(sep)
 }
 
+/**
+ * Responsible for representing the resolution using a unit-less number in scientific notation
+ * should probably ensure the mantissa has only on digit to the left of the decimal point.
+ *
+ * should it be taking a double or a string?
+ * @param number
+ */
+case class ScientificNotationTextFormatter(number: Double) extends TextFormatter {
+  val fmtdStr: String = lexiEncodeDoubleToString(number)
+  val numBits: Int = fmtdStr.length
+  def formatString(gh: GeoHash, dt: DateTime, sf: SimpleFeature) = fmtdStr
+}
+
+
+/**
+ * Responsible for representing the Band Number of a given raster
+ * have the bandNumber correspond to a description in the meta-data?
+ * like band 1 is RGB or is Elevation or is R or B or G?
+ * @param bandName
+ */
+case class RasterBandTextFormatter(bandName: String) extends TextFormatter {
+  val numBits: Int = bandName.length
+  def formatString(gh: GeoHash, dt: DateTime, sf: SimpleFeature) = bandName
+}

geomesa-core/src/main/scala/org/locationtech/geomesa/core/index/QueryPlanners.scala
@@ -345,6 +345,14 @@ case class ConstStringPlanner(cstr: String) extends KeyPlanner {
   }
 }
 
+case class ResolutionPlanner(ires: Double) extends KeyPlanner {
+  def getKeyPlan(filter:KeyPlanningFilter, output: ExplainerOutputType) = KeyListTiered(List(lexiEncodeDoubleToString(ires)))
+}
+
+case class BandPlanner(band: String) extends KeyPlanner {
+  def getKeyPlan(filter:KeyPlanningFilter, output: ExplainerOutputType) = KeyListTiered(List(band))
+}
+
 case class DatePlanner(formatter: DateTimeFormatter) extends KeyPlanner {
   val endDates = List(9999,12,31,23,59,59,999)
   val startDates = List(0,1,1,0,0,0,0)

geomesa-core/src/test/scala/org/locationtech/geomesa/core/index/IndexSchemaTest.scala
@@ -57,6 +57,55 @@ class IndexSchemaTest extends Specification {
       matched must beTrue
     }
 
+    "parse a valid string containing a positive resolution and positive exponent" in {
+      val res = 1.000000e+02
+      val s = s"%~#s%${lexiEncodeDoubleToString(res)}#ires%0,1#gh%99#r::%~#s%1,5#gh::%~#s%5,2#gh%15#id"
+      val matched = IndexSchema.buildKeyPlanner(s) match {
+        case CompositePlanner(List(ResolutionPlanner(100.0), GeoHashKeyPlanner(0,1), RandomPartitionPlanner(99)),"~") => true
+        case _ => false
+      }
+      matched must beTrue
+    }
+
+    "parse a valid string containing a negative resolution and positive exponent" in {
+      val res = -1.000000e+02
+      val s = s"%~#s%${lexiEncodeDoubleToString(res)}#ires%0,1#gh%99#r::%~#s%1,5#gh::%~#s%5,2#gh%15#id"
+      val matched = IndexSchema.buildKeyPlanner(s) match {
+        case CompositePlanner(List(ResolutionPlanner(-100.0), GeoHashKeyPlanner(0,1), RandomPartitionPlanner(99)),"~") => true
+        case _ => false
+      }
+      matched must beTrue
+    }
+
+    "parse a valid string containing a positive resolution and negative exponent" in {
+      val res = 1.000000e-02
+      val s = s"%~#s%${lexiEncodeDoubleToString(res)}#ires%0,1#gh%99#r::%~#s%1,5#gh::%~#s%5,2#gh%15#id"
+      val matched = IndexSchema.buildKeyPlanner(s) match {
+        case CompositePlanner(List(ResolutionPlanner(0.01), GeoHashKeyPlanner(0,1), RandomPartitionPlanner(99)),"~") => true
+        case _ => false
+      }
+      matched must beTrue
+    }
+
+    "parse a valid string containing a negative resolution and negative exponent" in {
+      val res = -1.000000e-02
+      val s = s"%~#s%${lexiEncodeDoubleToString(res)}#ires%0,1#gh%99#r::%~#s%1,5#gh::%~#s%5,2#gh%15#id"
+      val matched = IndexSchema.buildKeyPlanner(s) match {
+        case CompositePlanner(List(ResolutionPlanner(-0.01), GeoHashKeyPlanner(0,1), RandomPartitionPlanner(99)),"~") => true
+        case _ => false
+      }
+      matched must beTrue
+    }
+
+//    "parse a valid string containing Band CF" in {
+//      val s = "%~#s%0,1#gh%99#r::%~#s%1,5#gh%RGB#b::%~#s%5,2#gh%15#id"
+//      val matched = IndexSchema.buildColumnFamilyPlanner(s) match {
+//        case ColumnFamilyPlanner(List(GeoHashKeyPlanner(1,5), BandPlanner("RGB")),"~") => true
+//        case _ => false
+//      }
+//      matched must beTrue
+//    }
+
     "allow extra elements inside the column qualifier" in {
       val schema = Try(IndexSchema(
         "%~#s%foo#cstr%0,1#gh%99#r::%~#s%1,5#gh::%~#s%9#r%ColQ#cstr%15#id%5,2#gh",
@@ -192,6 +241,50 @@ class IndexSchemaTest extends Specification {
                    .nextPart()
                    .id()
                    .build()
+
+      schema must be equalTo oldSchema
+    }
+
+    "be able to create index schema with resolution" in {
+      val maxShard = 31
+      val name = "test"
+      val res = 100.0
+      val oldSchema = s"%~#s%$maxShard#r%$name#cstr%${lexiEncodeDoubleToString(res)}#ires%0,3#gh%yyyyMMdd#d::%~#s%3,2#gh::%~#s%#id"
+
+      val schema = new IndexSchemaBuilder("~")
+        .randomNumber(maxShard)
+        .constant(name)
+        .resolution(res)
+        .geoHash(0, 3)
+        .date("yyyyMMdd")
+        .nextPart()
+        .geoHash(3, 2)
+        .nextPart()
+        .id()
+        .build()
+
+      schema must be equalTo oldSchema
+    }
+
+    "be able to create index schema with resolution and band cf" in {
+      val maxShard = 31
+      val name = "test"
+      val res = 100.0
+      val band = "RGB"
+      val oldSchema = s"%~#s%$maxShard#r%$name#cstr%${lexiEncodeDoubleToString(res)}#ires%0,3#gh%yyyyMMdd#d::%~#s%3,2#gh%$band#b::%~#s%#id"
+
+      val schema = new IndexSchemaBuilder("~")
+        .randomNumber(maxShard)
+        .constant(name)
+        .resolution(100.0)
+        .geoHash(0, 3)
+        .date("yyyyMMdd")
+        .nextPart()
+        .geoHash(3, 2)
+        .band(band)
+        .nextPart()
+        .id()
+        .build()
 
       schema must be equalTo oldSchema
     }

commit b5d538567231eb54d8ac6a6672bab97687cc157c

GEOMESA-429.16 Raster Filtering Iterator
Modeled RFI after the AIFI, no tests.

includes fixes for issues introduced in 37d9e4a25fcc5b4b4e574e11db97d407e71290a3

geomesa-core/src/main/scala/org/locationtech/geomesa/core/core.scala
@@ -44,4 +44,8 @@ package object core {
   val GEOMESA_ITERATORS_TRANSFORM_SCHEMA    = "geomesa.iterators.transform.schema"
   val GEOMESA_ITERATORS_IS_DENSITY_TYPE     = "geomesa.iterators.is-density-type"
 
+  val RASTER_FEATURE_TYPE = "geomesa.raster.feature.type"
+  val RASTER_SCHEMA_NAME  = "geomesa.raster.index.schema"
+  val RASTER_FEATURE_NAME = "geomesa.raster.index.feature"
+
 }

geomesa-core/src/main/scala/org/locationtech/geomesa/core/index/IndexEntry.scala
@@ -1,7 +1,6 @@
 package org.locationtech.geomesa.core.index
 
 import java.nio.ByteBuffer
-
 import com.typesafe.scalalogging.slf4j.Logging
 import com.vividsolutions.jts.geom.Geometry
 import org.apache.accumulo.core.data.{Key, Value}
@@ -13,7 +12,6 @@ import org.locationtech.geomesa.core.data.{DATA_CQ, SimpleFeatureEncoder}
 import org.locationtech.geomesa.utils.geohash.{GeoHash, GeohashUtils}
 import org.locationtech.geomesa.utils.text.WKBUtils
 import org.opengis.feature.simple.SimpleFeature
-
 import scala.collection.JavaConversions._
 
 object IndexEntry {
@@ -60,10 +58,25 @@ object IndexEntry {
                encodedDtg)
   }
 
-  def decodeIndexValue(v: Value): DecodedIndexValue = {
-    val buf = v.get()
-    val idLength = ByteBuffer.wrap(buf, 0, 4).getInt
-    val (idPortion, geomDatePortion) = buf.drop(4).splitAt(idLength)
+  // the metadata CQ consists of the feature's:
+  // 1.  ID
+  // 2.  WKB-encoded geometry
+  // 3.  start-date/time
+  def encodeIndexCQMetadataKey(entry: SimpleFeature): Key = {
+    val encodedId = entry.sid.getBytes
+    val encodedGeom = WKBUtils.write(entry.geometry)
+    val encodedDtg = entry.dt.map(dtg => ByteBuffer.allocate(8).putLong(dtg.getMillis).array()).getOrElse(Array[Byte]())
+    val EMPTY_BYTES = Array.emptyByteArray
+    val cqByteArray = ByteBuffer.allocate(4).putInt(encodedId.length).array() ++ encodedId ++
+      ByteBuffer.allocate(4).putInt(encodedGeom.length).array() ++ encodedGeom ++
+      encodedDtg
+
+    new Key(EMPTY_BYTES, EMPTY_BYTES, cqByteArray, EMPTY_BYTES, Long.MaxValue)
+  }
+
+  def byteArrayToDecodedIndexValue(b: Array[Byte]): DecodedIndexValue = {
+    val idLength = ByteBuffer.wrap(b, 0, 4).getInt
+    val (idPortion, geomDatePortion) = b.drop(4).splitAt(idLength)
     val id = new String(idPortion)
     val geomLength = ByteBuffer.wrap(geomDatePortion, 0, 4).getInt
     if(geomLength < (geomDatePortion.length - 4)) {
@@ -73,8 +86,35 @@ object IndexEntry {
       DecodedIndexValue(id, WKBUtils.read(geomDatePortion.drop(4)), None)
     }
   }
+  
+  def byteArrayToDecodedMetadata(b: Array[Byte]): DecodedCQMetadata = {
+    // This will need to conform to some decided CQ format ie: gh(x,y) ~ metadata
+    // meta data being: id, geom, and time. below time is optional, may want to eliminate this choice
+    val idLength = ByteBuffer.wrap(b, 0, 4).getInt
+    val (idPortion, geomDatePortion) = b.drop(4).splitAt(idLength)
+    val id = new String(idPortion)
+    val geomLength = ByteBuffer.wrap(geomDatePortion, 0, 4).getInt
+    if(geomLength < (geomDatePortion.length - 4)) {
+      val (l,r) = geomDatePortion.drop(4).splitAt(geomLength)
+      DecodedCQMetadata(id, WKBUtils.read(l), Some(ByteBuffer.wrap(r).getLong))
+    } else {
+      DecodedCQMetadata(id, WKBUtils.read(geomDatePortion.drop(4)), None)
+    }
+  }
+
+  def decodeIndexValue(v: Value): DecodedIndexValue = {
+    val buf = v.get()
+    byteArrayToDecodedIndexValue(buf)
+  }
 
   case class DecodedIndexValue(id: String, geom: Geometry, dtgMillis: Option[Long])
+
+  def decodeIndexCQMetadata(k: Key): DecodedCQMetadata = {
+    val cqd = k.getColumnQualifierData.toArray
+    byteArrayToDecodedMetadata(cqd)
+  }
+
+  case class DecodedCQMetadata(id: String, geom: Geometry, dtgMillis: Option[Long])
 }
 
 case class IndexEntryEncoder(rowf: TextFormatter,
@@ -149,6 +189,24 @@ import org.locationtech.geomesa.core.index.IndexEntryDecoder._
 case class IndexEntryDecoder(ghDecoder: GeohashDecoder, dtDecoder: Option[DateDecoder]) {
   def decode(key: Key) = {
     val builder = localBuilder.get
+    builder.reset()
+    builder.addAll(List(ghDecoder.decode(key).geom, dtDecoder.map(_.decode(key))))
+    builder.buildFeature("")
+  }
+}
+
+object IndexCQMetadataDecoder {
+  val metaBuilder = new ThreadLocal[SimpleFeatureBuilder] {
+    override def initialValue(): SimpleFeatureBuilder = new SimpleFeatureBuilder(indexSFT)
+  }
+}
+
+import IndexCQMetadataDecoder._
+
+case class IndexCQMetadataDecoder(ghDecoder: GeohashDecoder, dtDecoder: Option[DateDecoder]) {
+  // are we grabbing the right stuff from the key?
+  def decode(key: Key) = {
+    val builder = metaBuilder.get
     builder.reset()
     builder.addAll(List(ghDecoder.decode(key).geom, dtDecoder.map(_.decode(key))))
     builder.buildFeature("")

geomesa-core/src/main/scala/org/locationtech/geomesa/core/index/IndexSchema.scala
@@ -372,6 +372,12 @@ object IndexSchema extends RegexParsers with Logging {
     val dateDecoder       = buildDateDecoder(s)
     IndexEntryDecoder(geohashDecoder, dateDecoder)
   }
+
+  def getIndexMetadataDecoder(s: String) = {
+    val geohashDecoder    = buildGeohashDecoder(s)
+    val dateDecoder       = buildDateDecoder(s)
+    IndexCQMetadataDecoder(geohashDecoder, dateDecoder)
+  }
 }
 
 /**

geomesa-core/src/main/scala/org/locationtech/geomesa/core/index/index.scala
@@ -132,7 +132,7 @@ package object index {
     }
   }
 
-  val doubleAlias = "double"//Double.getClass.getSimpleName.toLowerCase(Locale.US)
+  val doubleAlias = "double"
 
   def lexiEncodeDoubleToString(number: Double): String = LexiTypeEncoders.LEXI_TYPES.encode(number)
 

geomesa-core/src/main/scala/org/locationtech/geomesa/core/iterators/RasterFilteringIterator.scala
@@ -1,0 +1,93 @@
+/*
+ * Copyright 2014 Commonwealth Computer Research, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.locationtech.geomesa.core.iterators
+
+import java.util.{Date, Map => JMap}
+
+import com.typesafe.scalalogging.slf4j.Logging
+import com.vividsolutions.jts.geom.Geometry
+import org.apache.accumulo.core.data.{Key, Value}
+import org.apache.accumulo.core.iterators.{Filter, IteratorEnvironment, SortedKeyValueIterator}
+import org.geotools.feature.simple.SimpleFeatureBuilder
+import org.geotools.filter.text.ecql.ECQL
+import org.locationtech.geomesa.core._
+import org.locationtech.geomesa.core.index.IndexEntry.DecodedCQMetadata
+import org.locationtech.geomesa.core.index._
+import org.locationtech.geomesa.utils.geotools.SimpleFeatureTypes
+import org.opengis.feature.simple.SimpleFeature
+
+
+class RasterFilteringIterator extends Filter with Logging {
+
+  protected var filter: org.opengis.filter.Filter = null
+  protected var testSimpleFeature: SimpleFeature = null
+  protected var dateAttributeName: Option[String] = None
+
+  // NB: This is duplicated in the AIFI.  Consider refactoring.
+  lazy val wrappedSTFilter: (Geometry, Option[Long]) => Boolean = {
+    if (filter != null && testSimpleFeature != null) {
+      (geom: Geometry, olong: Option[Long]) => {
+        testSimpleFeature.setDefaultGeometry(geom)
+        for {
+          dateAttribute <- dateAttributeName
+          long <- olong
+        } {
+          testSimpleFeature.setAttribute(dateAttribute, new Date(long))
+        }
+        filter.evaluate(testSimpleFeature)
+      }
+    } else {
+      (_, _) => true
+    }
+  }
+
+  override def init(source: SortedKeyValueIterator[Key, Value],
+                    options: JMap[String, String],
+                    env: IteratorEnvironment) = {
+    super.init(source, options, env)
+
+    if (options.containsKey(DEFAULT_FILTER_PROPERTY_NAME) && options.containsKey(GEOMESA_ITERATORS_SIMPLE_FEATURE_TYPE)) {
+      val simpleFeatureTypeSpec = options.get(GEOMESA_ITERATORS_SIMPLE_FEATURE_TYPE)
+      val featureType = SimpleFeatureTypes.createType("RasterType", simpleFeatureTypeSpec)
+
+      featureType.decodeUserData(options, GEOMESA_ITERATORS_SIMPLE_FEATURE_TYPE)
+      dateAttributeName = getDtgFieldName(featureType)
+
+      val filterString = options.get(DEFAULT_FILTER_PROPERTY_NAME)
+      filter = ECQL.toFilter(filterString)
+      logger.debug(s"In RFI with $filter")
+      val sfb = new SimpleFeatureBuilder(featureType)
+      testSimpleFeature = sfb.buildFeature("test")
+
+    }
+  }
+
+  override def deepCopy(env: IteratorEnvironment) = {
+    val copy = super.deepCopy(env).asInstanceOf[RasterFilteringIterator]
+    copy.filter = filter
+    copy.testSimpleFeature = testSimpleFeature
+    copy
+  }
+
+  override def accept(k: Key, v: Value): Boolean = {
+    val DecodedCQMetadata(_, geom, dtgOpt) = IndexEntry.decodeIndexCQMetadata(k)
+    wrappedSTFilter(geom, dtgOpt)
+  }
+
+}
+
+object RasterFilteringIterator extends IteratorHelpers { }

geomesa-core/src/test/scala/org/locationtech/geomesa/core/index/FormattersTest.scala
@@ -50,7 +50,7 @@ class FormattersTest extends Specification {
 
     "map a double to a string representation" in {
       val scientificText = scientificNumberFormatter.formatString(null, null, null)
-      scientificText must be equalTo "1.234568e+08"
+      scientificText must be equalTo "c19d6f3454000000"
     }
   }
 